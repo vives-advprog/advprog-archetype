40985deefce0fe8a4254ceab9c93e40aa43799d8
Yves Seurynck <yves.seurynck@vives.be>
2021-05-17 11:31:14 +0200

    update advprog archetype

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -3,6 +3,6 @@ package ${package};
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Advanced Programming, ${artifactId}");
   }
 }
